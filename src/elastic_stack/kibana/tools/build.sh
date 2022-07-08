39248e778b9d3c032496eee43ad8e461cc538ffb
Null37 <ssamadi@student.1337.ma>
2022-07-08 13:25:39 +0000

    start metricbeat

--- src/elastic_stack/kibana/tools/build.sh
+++ src/elastic_stack/kibana/tools/build.sh
@@ -5,7 +5,7 @@ apt-get -y update && apt-get -y upgrade
 
 #installing utils
 
-apt-get -y install wget curl unzip
+apt-get -y install wget curl unzip vim sudo
 
 cd /home/elasticsearch
 
@@ -23,6 +23,26 @@ rm -rf config/kibana.yml
 cp /srcs/configs/kibana.yml ./config
 
 mkdir -p config/certs
+
+cd /home/elasticsearch
+
+curl --retry 10 -S -L --output /tmp/metricbeat.tar.gz https://artifacts.elastic.co/downloads/beats/metricbeat/metricbeat-8.3.1-linux-x86_64.tar.gz
+
+cd /tmp
+tar -zxf /tmp/metricbeat.tar.gz
+cd -
+
+rm -rf /tmp/metricbeat.tar.gz
+cp -rf /tmp/metricbeat* /home/elasticsearch/metricbeat-8.3.1
+
+cd metricbeat-8.3.1
+rm -rf config/metricbeat.yml
+cp /srcs/configs/metricbeat.yml ./config
+
+./metricbeat modules disable kibana
+./metricbeat modules enable kibana-xpack
+./metricbeat setup -e
+
 
 #adding user and group
 chown -R 1000:1000 /home/elasticsearch
--- src/elastic_stack/kibana/tools/entrypoint.sh
+++ src/elastic_stack/kibana/tools/entrypoint.sh
@@ -2,4 +2,9 @@ cd /home/elasticsearch/kibana-8.3.1
 
 curl -s -X POST --cacert config/certs/ca/ca.crt -u elastic:${ELASTIC_PASSWORD} -H "Content-Type: application/json" https://master-node:9200/_security/user/kibana_system/_password -d "{\"password\":\"${KIBANA_PASSWORD}\"}";
 
+echo "toor" | sudo -S chown root /home/elasticsearch/metricbeat-8.3.1/metricbeat.yml
+cd /home/elasticsearch/metricbeat-8.3.1
+echo "toor" | sudo -S ./metricbeat -e
+cd -
+
 exec bin/kibana
