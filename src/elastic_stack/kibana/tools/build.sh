a4bb703a81177a1778ed9d15e2c62a06792f3fab
root <root@elk-stack-1.us-west4-b.c.smooth-keel-354707.internal>
2022-07-08 17:29:45 +0000

    add metricbeat in kibana

--- src/elastic_stack/kibana/tools/build.sh
+++ src/elastic_stack/kibana/tools/build.sh
@@ -3,49 +3,41 @@
 #updating and upgrading packages
 apt-get -y update && apt-get -y upgrade
 
+
 #installing utils
+apt-get -y install coreutils wget curl unzip vim sudo
+apt-get -y install gnupg
+apt-get -y install systemctl
 
-apt-get -y install wget curl unzip vim sudo
+#install kibana
+wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | gpg --dearmor -o /usr/share/keyrings/elasticsearch-keyring.gpg
+apt-get -y install apt-transport-https
+echo "deb [signed-by=/usr/share/keyrings/elasticsearch-keyring.gpg] https://artifacts.elastic.co/packages/8.x/apt stable main" | sudo tee /etc/apt/sources.list.d/elastic-8.x.list
+apt-get -y update && apt-get -y install kibana=8.3.1
 
-cd /home/elasticsearch
 
-curl --retry 10 -S -L --output /tmp/kibana.tar.gz https://artifacts.elastic.co/downloads/kibana/kibana-8.3.1-linux-x86_64.tar.gz
 
-cd /tmp
-tar -zxf /tmp/kibana.tar.gz
-cd -
+#cd /home/elasticsearch
 
-rm -rf /tmp/kibana.tar.gz
-cp -rf /tmp/kibana* /home/elasticsearch/kibana-8.3.1
+#curl --retry 10 -S -L --output /tmp/kibana.tar.gz https://artifacts.elastic.co/downloads/kibana/kibana-8.3.1-linux-x86_64.tar.gz
 
-cd kibana-8.3.1
-rm -rf config/kibana.yml
-cp /srcs/configs/kibana.yml ./config
+#cd /tmp
+#tar -zxf /tmp/kibana.tar.gz
+#cd -
+
+#rm -rf /tmp/kibana.tar.gz
+#cp -rf /tmp/kibana* /home/elasticsearch/kibana-8.3.1
+
+cd /etc/kibana/
+rm -rf /etc/kibana/kibana.yml
+cp /srcs/configs/kibana.yml .
 
 mkdir -p config/certs
 
-cd /home/elasticsearch
-
-curl --retry 10 -S -L --output /tmp/metricbeat.tar.gz https://artifacts.elastic.co/downloads/beats/metricbeat/metricbeat-8.3.1-linux-x86_64.tar.gz
-
-cd /tmp
-tar -zxf /tmp/metricbeat.tar.gz
-cd -
-
-rm -rf /tmp/metricbeat.tar.gz
-cp -rf /tmp/metricbeat* /home/elasticsearch/metricbeat-8.3.1
-
-cd metricbeat-8.3.1
-rm -rf config/metricbeat.yml
-cp /srcs/configs/metricbeat.yml ./config
-
-./metricbeat modules disable kibana
-./metricbeat modules enable kibana-xpack
-./metricbeat setup -e
 
 
 #adding user and group
-chown -R 1000:1000 /home/elasticsearch
+#chown -R 1000:1000 /home/elasticsearch
 
 
 
--- src/elastic_stack/kibana/tools/entrypoint.sh
+++ src/elastic_stack/kibana/tools/entrypoint.sh
@@ -1,10 +1,32 @@
-cd /home/elasticsearch/kibana-8.3.1
+#cd /home/elasticsearch/kibana-8.3.1
 
-curl -s -X POST --cacert config/certs/ca/ca.crt -u elastic:${ELASTIC_PASSWORD} -H "Content-Type: application/json" https://master-node:9200/_security/user/kibana_system/_password -d "{\"password\":\"${KIBANA_PASSWORD}\"}";
+curl -s -X POST --cacert /etc/kibana/config/certs/ca/ca.crt -u elastic:${ELASTIC_PASSWORD} -H "Content-Type: application/json" https://master-node:9200/_security/user/kibana_system/_password -d "{\"password\":\"${KIBANA_PASSWORD}\"}";
 
-echo "toor" | sudo -S chown root /home/elasticsearch/metricbeat-8.3.1/metricbeat.yml
+curl --retry 10 -S -L --output /tmp/metricbeat.tar.gz https://artifacts.elastic.co/downloads/beats/metricbeat/metricbeat-8.3.1-linux-x86_64.tar.gz
+
+cd /tmp
+tar -zxf /tmp/metricbeat.tar.gz
+#cd -
+
+rm -rf /tmp/metricbeat.tar.gz
+cp -rf /tmp/metricbeat* /home/elasticsearch/metricbeat-8.3.1
+
 cd /home/elasticsearch/metricbeat-8.3.1
-echo "toor" | sudo -S ./metricbeat -e
-cd -
+rm -rf metricbeat.yml
+cp /srcs/configs/metricbeat.yml .
 
-exec bin/kibana
+
+systemctl enable kibana.service
+systemctl start kibana.service
+
+sleep 60
+
+./metricbeat modules disable kibana
+./metricbeat modules disable system
+./metricbeat modules enable kibana-xpack
+
+
+rm -rf modules.d/kibana-xpack.yml
+cp /srcs/configs/kibana-xpack.yml ./modules.d/.
+./metricbeat setup -e
+./metricbeat -e
