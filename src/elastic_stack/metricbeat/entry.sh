ebd0417c67072c9f4aab6eba311d64d80e3292fd
Null37 <ssamadi@student.1337.ma>
2022-07-09 18:17:13 +0000

    kibana and elastic search monitoring done

--- src/elastic_stack/metricbeat/entry.sh
+++ src/elastic_stack/metricbeat/entry.sh
@@ -1,7 +1,18 @@
 cd /root/metricbeat-8.3.1
 
-sleep 300
+x=0
+while [ $x -le 30 ]
+do
+	curl -s -I http://kibana:5601 | grep '302 Found'
+	if [ $? -eq 0 ];
+	then
+		./metricbeat setup -e
+		./metricbeat -e
+        	break
+        fi
+        echo "Still waiting for kibana to start"
+        sleep 10
+        x=$(( $x + 1 ))
+done
 
-./metricbeat setup -e
-./metricbeat -e
 
